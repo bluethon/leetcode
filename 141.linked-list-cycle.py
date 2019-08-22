d71eaca8856ed9c5ec8a09fb644575f39efcbca6
bluethon <j5088794@gmail.com>
2019-08-22 22:13:01 +0800

    algo - 141 patch1

--- 141.linked-list-cycle.py
+++ 141.linked-list-cycle.py
@@ -15,12 +15,15 @@ class Solution(object):
         :type head: ListNode
         :rtype: bool
         """
+        # #1 可以不边界检测, 但是下面需要验证slow和fast
         slow = fast = head
         # 注意检测slow和fast
+        # #1 只需要到fast.next, 不需要fast.next.next
         while slow and fast and fast.next:
             slow = slow.next
             fast = fast.next.next
             # 使用is, 不用==
+            # #1 is
             if slow is fast:
                 return True
         return False
